0aef510f56ae2f9e50301367e17b55aa86ec9212
mutegi <mutegisamuel0@gmail.com>
2018-01-26 00:11:05 +0300

    added a custom font

--- app/src/main/java/com/example/humungus/infomavel/MainActivity.java
+++ app/src/main/java/com/example/humungus/infomavel/MainActivity.java
@@ -1,19 +1,29 @@
 package com.example.humungus.infomavel;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 //    splashscreen timmer
-    private static int SPLASH_TIME_OUT = 4000;
+    private static int SPLASH_TIME_OUT = 2000;
+
+    private TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        adding custom font
+        mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
+
+        Typeface ostrichFont = Typeface.createFromAsset(getAssets(), "fonts/AVENGEANCE.ttf");
+        mAppNameTextView.setTypeface(ostrichFont);
 
 //        splashscreen code
         new Handler().postDelayed(new Runnable() {
